0493633b9cc0de2f53a1ed4ad09bada77074f0ab
Phil J. Laszkowicz <phil@fillip.pro>
2017-12-21 17:43:31 +0200

    Corrected name of project in package.

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "sivukartta",
+    name: "Sivukartta",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "sivukartta",
-            targets: ["sivukartta"]),
+            name: "Sivukartta",
+            targets: ["Sivukartta"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "sivukartta",
+            name: "Sivukartta",
             dependencies: []),
         .testTarget(
-            name: "sivukarttaTests",
-            dependencies: ["sivukartta"]),
+            name: "SivukarttaTests",
+            dependencies: ["Sivukartta"]),
     ]
 )
